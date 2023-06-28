f99c4d8ff092b201c65a06df3fd056322466d8be
Dajve Echols <edajve@gmail.com>
2023-06-28 13:55:43 -0500

    add localSecrets

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const { writeFile } = require('fs')
 const { coverMaker } = require('./cover-creator')
+const { localSecrets } = require('./secrets/ppi')
 
 const filePath = 'cover_letter.txt'
 
 writeFile(filePath, coverMaker, (err, result) => {
     if (err) return `Error: ${err.message}`
     console.log("Written successfully..")
+  
 })
+
+console.log(localSecrets.pass)
