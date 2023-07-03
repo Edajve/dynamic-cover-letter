306aabe62305fff828c4d27b78f145fecee7f2bb
Dajve Echols <edajve@gmail.com>
2023-07-03 18:26:52 -0500

    working prototype

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const { writeFile } = require('fs')
-const coverLetter = require('./cover-creator')
+const { writeFile, readFile } = require('fs').promises
+const { createWriteStream } = require('fs')
+const coverMaker = require('./cover-creator')
 const { localSecrets } = require('./secrets/ppi')
 const formatDate = require('./helpers')
+const officegen = require('officegen');
 
 const template = {
-    companyName: "test",
-    positionTitle: "tester tester",
+    companyName: "skye",
+    positionTitle: "cheater",
     recipientsJobTitle: "HR Department",
     fullName: "Dajve Echols",
     address: localSecrets.address,
@@ -20,9 +22,57 @@ const template = {
     github: "https://github.com/Edajve"
 }
 
-const filePath = 'cover_letter.txt'
+const testTemplate = {
+    companyName: "test",
+    positionTitle: "tester tester",
+    recipientsJobTitle: "test",
+    fullName: "test",
+    address: "test",
+    state: "test",
+    city: "test",
+    zipCode: "test",
+    phoneNum: "test",
+    linkedIn: "test",
+    date: "test",
+    recipientsName: "test",
+    emailAddress: "test",
+    github: "test"
+}
 
-writeFile(filePath, coverLetter(template), (err, result) => {
-    if (err) return `Error: ${err.message}`
-    console.log("Written successfully..")
-})
+const filePath = 'cover_letter.txt'
+const outputDirectory = '/Users/dajve.echols/Desktop/cover_letters'
+
+const writeToExcel = async (path, coverFunc, template) => {
+    try {
+        const coverMaker = coverFunc;
+        await writeFile(path, coverMaker(template))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const saveDocument = async (text, fileToSave, companyName) => {
+     const docx = officegen('docx');
+
+     const paragraph = docx.createP();
+     paragraph.addText(text);
+ 
+     const fileName = `${companyName}-cover-letter.docx`;
+     const outputPath = `${fileToSave}/${fileName}`;
+     const outputStream = createWriteStream(outputPath);
+     docx.generate(outputStream);
+     outputStream.on('close', function() {
+        console.log('Output file saved successfully.');
+     });
+}
+
+const readFileFromExcel = (theFilePath, outputDirectory, companyName ) => {
+    readFile(theFilePath, 'utf-8')
+    .then(result => {
+        saveDocument(result, outputDirectory, companyName)
+    })
+    .catch(err => console.log(err.message)) 
+}
+
+writeToExcel(filePath, coverMaker, template)
+readFileFromExcel(filePath, outputDirectory, template.companyName)
