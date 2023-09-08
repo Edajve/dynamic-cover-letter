8392882a6fb44a12fed51512ede21e1ae5fe7316
Edajvee <dechols@fubo.tv>
2023-09-08 01:22:02 -0500

    add logic to update the text file and reset daily

--- db_Operations.js
+++ db_Operations.js
@@ -1,33 +1,38 @@
 const { formatDate } = require('./helpers')
+const fs = require('fs');
 
 const updateDB = async (nameOfCompany) => {
-  const dbPath = "./DB.txt";
+  const dbPath = "/Users/dajveechols/remoteSrc/node.js/dynamic-cover-letter/db/DB.txt";
+
 
   try {
-    const response = await readFile(dbPath, 'utf-8');
-    const json = JSON.parse(response);
+    const json = await fs.promises.readFile(dbPath, 'utf-8');
+    const dbJson = JSON.parse(json);
 
+    const length = dbJson.length - 1
     const todaysDate = formatDate();
-    const companies = json.applications;
+    const companies = dbJson[length].dailyStats.applications;
+    const latestDbDate = dbJson[length].today
+  
 
-    if (json.today === todaysDate) {
-      companies.push(nameOfCompany);
-      json.dailyStats.applicationAmount = companies.length;
+    if (latestDbDate === todaysDate) {
+      companies.push(nameOfCompany); // Use push without reassigning
+      dbJson[length].dailyStats.applicationAmount = companies.length; //this is not being updated
     } else {
       const newObject = {
         today: todaysDate,
         dailyStats: {
           applicationAmount: 1,
-          applications: []
+          applications: [nameOfCompany]
         }
       };
-      newObject.dailyStats.applicationAmount.push(nameOfCompany)
-      data.push(newObject);
+      dbJson.push(newObject)
     }
 
-    const updatedJsonData = JSON.stringify(json, null, 2);
+    const updatedJsonData = JSON.stringify(dbJson, null, 2);
 
-    await writeFile(dbPath, updatedJsonData);
+    await fs.promises.writeFile(dbPath, updatedJsonData);
+
     console.log('Database updated successfully.');
   } catch (err) {
     console.error(err.message);
